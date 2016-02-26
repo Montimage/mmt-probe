5e9c62a1f6259139ff866dba05ac94f3f198507c
jeevan.pokhrel <jeevanpokhrel@gmail.com>
2016-02-26 11:19:58 +0100

    built script updates

--- built.sh
+++ built.sh
@@ -3,34 +3,34 @@ sudo gcc -g -o /usr/bin/offlineprobe src/smp_main.c  src/processing.c src/web_se
 
 sudo gcc -g -o /usr/bin/onlineprobe src/smp_main.c  src/processing.c src/web_session_report.c src/thredis.c src/send_msg_to_file.c src/send_msg_to_redis.c src/rtp_session_report.c src/event_based_reporting.c src/protocols_report.c src/ssl_session_report.c src/default_app_session_report.c src/microflows_session_report.c src/radius_reporting.c src/security_analysis.c src/parseoptions.c src/license.c  src/ftp_session_report.c src/ip_statics.c -lmmt_core -lmmt_tcpip -lmmt_security -lxml2 -ldl -lpcap -lconfuse -lhiredis -lpthread
 
-if [ ! -d /temp/ ]
+if [ ! -d /tmp/ ]
 then
-    mkdir /temp/
+    mkdir /tmp/
 fi
-if [ ! -d /temp/reports_online ]
+if [ ! -d /tmp/reports_online ]
 then
-    mkdir /temp/reports_online/
+    mkdir /tmp/reports_online/
 fi
-if [ ! -d /temp/reports_offline/ ]
+if [ ! -d /tmp/reports_offline/ ]
 then
-    mkdir /temp/reports_offline/
+    mkdir /tmp/reports_offline/
 fi
-if [ ! -d /temp/behaviour_reports_online/ ]
+if [ ! -d /tmp/behaviour_reports_online/ ]
 then
-    mkdir /temp/behaviour_reports_online/
+    mkdir /tmp/behaviour_reports_online/
 fi
-if [ ! -d /temp/behaviour_reports_offline/ ]
+if [ ! -d /tmp/behaviour_reports_offline/ ]
 then
-    mkdir /temp/behaviour_reports_offline/
+    mkdir /tmp/behaviour_reports_offline/
 fi
-if [ ! -d /temp/security_reports_online/ ]
+if [ ! -d /tmp/security_reports_online/ ]
 then
-    mkdir /temp/security_reports_online/
+    mkdir /tmp/security_reports_online/
 fi
 
-if [ ! -d /temp/security_reports_offline/ ]
+if [ ! -d /tmp/security_reports_offline/ ]
 then
-    mkdir /temp/security_reports_offline/
+    mkdir /tmp/security_reports_offline/
 fi
 
 if [ ! -d /etc/mmt/ ]
@@ -41,6 +41,7 @@ fi
 sudo cp mmt_online.conf /etc/mmt/
 sudo cp mmt_offline.conf /etc/mmt/
 sudo cp License_key.txt /etc/mmt/
+sudo cp log2.data /etc/mmt/
 
 
 sudo cp run_mmt_offline /etc/init.d/
