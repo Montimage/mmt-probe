4c7936eeb58872019c21fc2e07b9ced74279fe59
Huu Nghia NGUYEN <huunghia.nguyen@montimage.com>
2016-06-14 15:15:31 +0200

    hash packet by IPs and Ports

--- src/lib/packet_hash.c
+++ src/lib/packet_hash.c
@@ -6,20 +6,19 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <arpa/inet.h>
 #include "packet_hash.h"
 #include "optimization.h"
 
-#define __HASH_TABLE_SIZE 10000
+#define __HASH_TABLE_SIZE 100000
 
 
 
-static uint32_t _dispatcher( uint32_t nu ){
-	static uint32_t hash_table[ __HASH_TABLE_SIZE ];
+static uint32_t _dispatcher( uint64_t nu ){
+	static uint64_t hash_table[ __HASH_TABLE_SIZE ];
 	static uint32_t length = 0;
 	static uint32_t i;
-
-	if( unlikely( length >= __HASH_TABLE_SIZE )) length = 0;
 
 	//check if this number exists in the hash table
 	for( i=0; i<length; i++ )
@@ -29,6 +28,10 @@ static uint32_t _dispatcher( uint32_t nu ){
 	//if not, add it
 	hash_table[ length ] = nu;
 	length ++;
+
+	if( unlikely( length >= __HASH_TABLE_SIZE ))
+		length = 0;
+
 	return length - 1;
 }
 
@@ -49,7 +52,9 @@ uint32_t get_packet_hash_number( const u_char *packet, size_t len ){
 
 	static uint32_t a1, a2;
 	static uint16_t p1, p2;
-	static uint16_t ip_src_off, ip_dst_off, port_src_off;
+	static uint16_t ip_src_off;
+	static uint8_t proto_id;
+
 	if (len < 38) return 0; //TODO: this is not elegant check IP, IPv6 etc.
 
 	if( packet == NULL)
@@ -60,26 +65,28 @@ uint32_t get_packet_hash_number( const u_char *packet, size_t len ){
 
 	if ( likely( ntohs(eth->h_proto) == 0x0800 ) ) {
 		ip_src_off = 26;
-		ip_dst_off = 30;
-		port_src_off = 38;
-	} else if ( likely(ntohs(eth->h_proto) == 0x8100)) {
+	}
+	//vlan
+	else if ( likely(ntohs(eth->h_proto) == 0x8100)) {
 		ip_src_off = 30;
-		ip_dst_off = 34;
-		port_src_off = 42;
 	} else {
 		return 0;
 	}
 
 	a1 = *((uint32_t *) &packet[ip_src_off]);
-	a2 = *((uint32_t *) &packet[ip_dst_off]);
+	a2 = *((uint32_t *) &packet[ip_src_off + 4]);
 
-	p1 = *((uint16_t *) &packet[port_src_off]);
-	p2 = *((uint16_t *) &packet[port_src_off + 1]);
+	proto_id = *((uint8_t *) &packet[ ip_src_off - 3 ]);
+	//src and dst ports of TCP or UDP
+	if( likely(proto_id == 6 || proto_id == 17 )){
+		p1 = *((uint16_t *) &packet[ ip_src_off + 8]);
+		p2 = *((uint16_t *) &packet[ ip_src_off + 8 + 2]);
+	}
+	else
+		p1 = p2 = 0;
 
-	p1 = p2 = 0;
-
-	//p1 = ntohl( p1 );
-	//p2 = ntohl( p2 );
+	//p1 = ntohs( p1 );
+	//p2 = ntohs( p2 );
 
 //	if ((ntohl(a1) & 0xFF000000 /* 255.0.0.0 */) == 0x0A000000 /* 10.0.0.0 */) {
 //		return ntohl(a1);
@@ -90,18 +97,17 @@ uint32_t get_packet_hash_number( const u_char *packet, size_t len ){
 //	}
 //
 	/*
-	printf("src: %03d.%03d.%03d.%03d, dst: %03d.%03d.%03d.%03d, port_src: %d, port_dst: %d, %d %d\n",
+	printf("%s src: %03d.%03d.%03d.%03d, dst: %03d.%03d.%03d.%03d, port_src: %d, port_dst: %d\n",
+			proto_id == 6? "TCP" :( proto_id == 17? "UDP" : "---"),
 			(a1 >> 24), ((a1 << 8) >> 24), ((a1 << 16) >> 24), ((a1 << 24) >> 24),
 			(a2 >> 24), ((a2 << 8) >> 24), ((a2 << 16) >> 24), ((a2 << 24) >> 24),
-			p1, p2
-			, packet[port_src_off], packet[port_src_off + 1]
+			htons( p1 ), htons( p2 )
 			);
-	exit( 0 );
+	//exit( 0 );
 	*/
 	//a1 = (a1 & a2) ^ (a1 | a2);
 //	a1 = (a1 >> 24) + (a1 >> 16) + (a1 >> 8) + a1;
 //	a2 = (a2 >> 24) + (a2 >> 16) + (a2 >> 8) + a2;
-	a1 = a1 + a2 + p1 + p2;
 
-	return _dispatcher( a1 );
+	return _dispatcher( a1 + a2 + p1 + p2 );
 }
