64574d4d0cd729abc59e908d1831b294540bb7e6
huunghia <huunghia.nguyen@montimage.com>
2023-07-20 15:12:16 +0200

    project-mosaico: add scripts to generate .deb file [skip ci]

---
+++ build-mosaico-deb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp mosaico.conf mmt-probe.conf
+make clean
+make SOCKET_MODULE compile
+make SOCKET_MODULE deb
+
+# to test using docker ==> use ubuntu 22.10
+# docker run --rm -it -v$PWD:/tmp/a ubuntu:22.10
